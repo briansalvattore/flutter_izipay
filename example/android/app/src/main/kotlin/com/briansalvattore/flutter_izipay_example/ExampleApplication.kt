81d4511af6f147fbf05a53ad34149a897da0250c
Brian Salvattore <briansalvattore@gmail.com>
2024-12-21 02:05:55 -0300

    complete example

---
+++ example/android/app/src/main/kotlin/com/briansalvattore/flutter_izipay_example/ExampleApplication.kt
@@ -0,0 +1,7 @@
+package com.briansalvattore.flutter_izipay_example
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExampleApplication : Application()
